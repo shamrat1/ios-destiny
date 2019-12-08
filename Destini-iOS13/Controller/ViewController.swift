e1b703348402e21a327c34494bcb24236e212d35
Yasin Shamrat <yshamrat@gmail.com>
2019-12-08 13:08:03 +0600

    Story app challenge from london app brewery completed with dark mode applied

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var story = StoryBrain()
+    var storyLine = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        nextStory()
     }
 
-
+    @IBAction func choiceAction(_ sender: UIButton) {
+        if sender.tag == 1{
+            let destination = story.list[storyLine].choice1Destination
+            storyLine = destination
+        }else {
+            let destination = story.list[storyLine].choice2Destination
+            storyLine = destination
+        }
+        nextStory()
+    }
+    
+    func nextStory() -> Void {
+        storyLabel.text = story.list[storyLine].title
+        choice1Button.setTitle(story.list[storyLine].choice1, for: .normal)
+        choice2Button.setTitle(story.list[storyLine].choice2, for: .normal)
+    }
 }
 
